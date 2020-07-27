5761da954f520f5502f93d8fb33c46a843de6740
Federico <toledofederico11@gmail.com>
2020-07-26 22:54:23 -0300

    refactor column names

--- api/models.py
+++ api/models.py
@@ -4,21 +4,20 @@ from django.urls import reverse
 
 class User(models.Model):
     uid = models.CharField(max_length=35, unique=True)
-    fullName = models.CharField(max_length=30)
+    fullname = models.CharField(max_length=30)
     username = models.CharField(max_length=30, unique=True)
-    userPicture = models.ImageField(upload_to=None, null=True)
+    user_picture = models.ImageField(upload_to=None, null=True)
     email = models.EmailField(max_length=50, unique=True)
     password = models.CharField(max_length=30)
-    phoneNumber = models.CharField(max_length=10)
+    phone_number = models.CharField(max_length=10)
 
     def __str__(self):
         return self.username
     
-
 class Market(models.Model):
     name = models.CharField(max_length=30)
     description = models.TextField(null=True)
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     logo = models.ImageField(upload_to=None, null=True)
 
     def __str__(self):
@@ -38,19 +37,18 @@ class Product(models.Model):
 class ProductImage(models.Model):
     product = models.ForeignKey(Product, related_name='images', on_delete=models.CASCADE)
     image = models.ImageField(upload_to='temp/')
-    remoteURL = models.CharField(max_length=200, null=True)
+    remote_url = models.CharField(max_length=200, null=True)
 
 class Favourites(models.Model):
-    useruid = models.CharField(max_length=35)
-    productID = models.IntegerField()
+    user_uid = models.CharField(max_length=35)
+    product_id = models.IntegerField()
 
 class Rated(models.Model):
-    useruid = models.CharField(max_length=35)
-    productID = models.IntegerField()
+    user_uid = models.CharField(max_length=35)
+    product_id = models.IntegerField()
 
 class Transaction(models.Model):
-    useruid = models.CharField(max_length=35)
-    marketID = models.IntegerField()
-    productID = models.IntegerField()
+    user_uid = models.CharField(max_length=35)
+    market_id = models.IntegerField()
+    product_id = models.IntegerField()
     quantity = models.IntegerField()
-    totalPrice = models.DecimalField(max_digits=6, decimal_places=2)
