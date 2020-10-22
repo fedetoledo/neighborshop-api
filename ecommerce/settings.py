c7d3d1f1e9ebdcc6fd0fc53e21809393d66b6bc0
Federico <toledofederico11@gmail.com>
2020-10-22 09:35:08 -0300

    Change in market processor name

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -90,7 +90,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'control_panel.context_processors.market',
+                'control_panel.context_processors.user_market_data',
             ],
         },
     },
